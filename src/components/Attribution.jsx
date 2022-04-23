0e1ef32e49dfa587a3a9e85e8ddd2d1cb4439df4
Kevin Brian <realkevinbrian@gmail.com>
2022-04-23 09:39:44 +0200

    fixed some bugs

--- src/components/Attribution.jsx
+++ src/components/Attribution.jsx
@@ -25,7 +25,7 @@ function Attribution() {
   return (
     <StyledAttribution>
     Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.<br/> 
-    Coded by <a href="http://github.com/realkevinbrian">RealKevinBrian</a>.
+    Coded by <a href="http://github.com/realkevinbrian">RealKevinBrian </a> 2022.
   </StyledAttribution>
   )
 }
